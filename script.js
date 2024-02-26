7fee5c60f986c3c6f596cf070c104bf8be2bdf72
skfardin2k27 <imsfn2709@gmail.com>
2024-02-26 19:46:30 +0530

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+
+setInterval(function() {
+    const date= new Date()
+
+    hours.textContent = date.getHours()
+    minutes.textContent = date.getMinutes()
+    seconds.textContent = date.getSeconds()
+})
